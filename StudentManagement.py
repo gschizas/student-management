e4f808b65b88ab1cb904a8b6fbb279be133d0a7a
George Schizas <gschizas@gmail.com>
2017-08-11 19:47:49 +0300

    Move reports to their own folder

--- StudentManagement.py
+++ StudentManagement.py
@@ -73,7 +73,7 @@ class ReportsView(BaseView):
             student_paid = sum([p.amount for p in all_payments if p.student_id == s.id])
             student_bought = sum([l.fee * l.hours for l in all_lessons if l.student_id == s.id])
             lines.append({'Student': str(s), 'Balance': round(student_bought - student_paid, 2)})
-        return self.render('reports_index.html', lines=lines)
+        return self.render('reports/index.html', lines=lines)
 
 
 def _current_year():
